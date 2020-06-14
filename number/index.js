20d70882a7708683ae8685f2ba7706eb2955a891
Siaoja <1610692147@qq.com>
2020-06-14 15:18:13 +0800

    complete two READ.md over.

---
+++ number/index.js
@@ -0,0 +1,9 @@
+for(let i = 0; i <= 20; i++){
+    if(i%2 == 0){
+        //偶数
+        console.log(i + ' is odd number.');
+    }else if(i%2 == 1){
+        //奇数
+        console.log(i + ' is even number.');
+    }
+}
